fdab64c5bd825be2a9f8d4c925616b482ed53ec3
MrJonoCES <83773607+MrJonoCES@users.noreply.github.com>
2024-11-15 15:15:55 +0000

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,43 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+const env = loadEnv("", process.cwd(), "STORYBLOK");
+import robots from "astro-robots";
+import icon from "astro-icon";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
+  site: "https://janefaram.com",
+  prefetch: true,
+  image: {
+    remotePatterns: [{ protocol: "https" }],
+  },
+  integrations: [
+    (await import("@playform/compress")).default({
+      CSS: false,
+      Image: false,
+    }),
+    robots(),
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        page: "storyblok/Page",
+        richTextTest: "storyblok/RichTextTest",
+      },
+      apiOptions: {
+        // Choose your Storyblok space region
+        // region: "us", // optional,  or 'eu' (default)
+      },
+    }),
+    icon(),
+    sitemap(),
+  ],
+  build: {
+    assets: "assets", // Specify assets directory
+  },
+});
